b732f3f02d49e74ee898f7d369c39bd631e9a6bc
EBMEHOB <ebmehob@gmail.com>
2023-06-10 16:13:22 +0300

    Added DSP

--- Source/PluginProcessor.h
+++ Source/PluginProcessor.h
@@ -62,6 +62,15 @@ public:
     juce::AudioProcessorValueTreeState apvts{*this, nullptr, "Parametrs", createParameterLayout()};
 
 private:
+    using Filter = juce::dsp::IIR::Filter<float>;
+    
+    using CutFilter = juce::dsp::ProcessorChain<Filter, Filter, Filter, Filter>;
+    
+    using MonoChain = juce::dsp::ProcessorChain<CutFilter, Filter, CutFilter>;
+    
+    MonoChain leftChain, rightChain;
+    
+    
     //==============================================================================
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (FirstEQAudioProcessor)
 };
